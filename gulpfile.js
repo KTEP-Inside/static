fac848d277bd3a14f6212b2b44320ea47d4713e0
Bricks <76704453+Bricks666@users.noreply.github.com>
2023-03-18 18:06:40 +0300

    Добавление шрифтов

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const gulp = require("gulp");
-const rimraf = require("rimraf");
-const rename = require("gulp-rename");
+const gulp = require('gulp');
+const rimraf = require('rimraf');
+const rename = require('gulp-rename');
 
-const postcss = require("gulp-postcss");
-const postcssImport = require("postcss-import");
-const postcssCsso = require("postcss-csso");
-const postcssMinMax = require("postcss-media-minmax");
+const postcss = require('gulp-postcss');
+const postcssImport = require('postcss-import');
+const postcssCsso = require('postcss-csso');
+const postcssMinMax = require('postcss-media-minmax');
 
 /**
  * Processing CSS
@@ -17,32 +17,30 @@ const postcssMinMax = require("postcss-media-minmax");
 const getPostcssTask = () =>
 	postcss([postcssImport, postcssCsso, postcssMinMax]);
 
-gulp.task("styles::base::postcss", () => {
+gulp.task('styles::base::postcss', () => {
 	return gulp
-		.src("src/styles/styles.css")
+		.src('src/styles/styles.css')
 		.pipe(getPostcssTask())
-		.pipe(rename("styles/styles.min.css"))
-		.pipe(gulp.dest("dist/"));
+		.pipe(rename('styles/styles.min.css'))
+		.pipe(gulp.dest('dist/'));
 });
 
-gulp.task("styles::schemes::postcss", () => {
+gulp.task('styles::schemes::postcss', () => {
 	return gulp
-		.src("src/styles/schemes/*.css")
+		.src('src/styles/schemes/*.css')
 		.pipe(getPostcssTask())
-		.pipe(gulp.dest("dist/styles/schemes"));
+		.pipe(gulp.dest('dist/styles/schemes'));
 });
 
-gulp.task("clean", () => {
-	return rimraf(["dist/styles/styles.css"], {
-		glob: true,
-	});
+gulp.task('fonts::copy', () => {
+	return gulp.src('src/fonts/**').pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task(
-	"build",
+	'build',
 	gulp.series(
-		"styles::base::postcss",
-		"styles::schemes::postcss",
-		"clean"
+		'styles::base::postcss',
+		'styles::schemes::postcss',
+		'fonts::copy'
 	)
 );
